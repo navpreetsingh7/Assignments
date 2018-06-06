16226ef4454cab612aca96daecb97539bb1322e3
NAVPREET SINGH <navbajwa7@gmail.com>
2018-06-06 23:11:14 +0530

    started assignment

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+#A2_Q1
+print("hello word")
+
+#A2_Q2
+print("acad"+"view")
+
+#A2_Q3
+x=10
+y=20
+z=30
+print(x,y,z)
+
+#A2_Q4
+print("let\'s get started")
+
+#A2_Q5
+s="ACADVIEW"
+course="python"
+fees=5000
+print("hi,%s my couse is :%s, my fees is %s"%(s,course,fees))
+
+#aA2_Q6
+name="Tony Stark"
+salary=1000000
+print("hi %s, my %d"%(name,salary))
